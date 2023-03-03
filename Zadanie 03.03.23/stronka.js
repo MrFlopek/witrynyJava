f218fa10ddd75d1a3b56610d1196157dc4aa102a
MrFlopek <milolajfilipczak@gmail.com>
2023-03-03 10:24:16 +0100

    03.03.2023

---
+++ Zadanie 03.03.23/stronka.js
@@ -0,0 +1,30 @@
+//1
+const jeden = document.querySelector("#s1")
+jeden.addEventListener('click', function () {
+    document.querySelector("#s1").innerHTML = "&#8613"
+});
+console.log(jeden)
+//2
+const dwa = document.querySelector("#s2")
+dwa.addEventListener('click', function () {
+    document.querySelector("#s2").innerHTML = "&#8613"
+});
+console.log(dwa)
+//3
+const trzy = document.querySelector("#s3")
+trzy.addEventListener('click', function () {
+    document.querySelector("#s3").innerHTML = "&#8613"
+});
+console.log(trzy)
+//4
+const cztery = document.querySelector("#s4")
+cztery.addEventListener('click', function () {
+    document.querySelector("#s4").innerHTML = "&#8613"
+});
+console.log(cztery)
+//5
+const piec = document.querySelector("#s5")
+piec.addEventListener('click', function () {
+    document.querySelector("#s5").innerHTML = "&#8613"
+});
+console.log(piec)
