dc719785565291c53f7c7327dbd84cb7403c4bc9
MrFlopek <milolajfilipczak@gmail.com>
2023-03-24 10:26:41 +0100

    24.03.2023

---
+++ zadanie 24.03.2023/zarowka.js
@@ -0,0 +1,13 @@
+document.querySelector("#dwa").addEventListener("click", function () {
+    document.getElementById("zarowkazdj").src="zarowka1.jpg";
+    document.getElementById("radiodwa").checked="false";
+    document.getElementById("checkboxjeden").checked="false";
+});
+document.querySelector("#jeden").addEventListener("click", function () {
+    document.getElementById("zarowkazdj").src="zarowka2.jpg";
+    document.getElementById("radiojeden").checked="true";
+    document.getElementById("checkboxjeden").checked="true";
+});
+document.querySelector('#checkboxjeden').addEventListener('click', function (){
+    document.getElementById('zarowkazdj').src="zarowka2.jpg"
+})
