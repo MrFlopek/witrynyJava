c6cc2066b8923f6ec5c4fccfa391677a503aa744
MrFlopek <milolajfilipczak@gmail.com>
2023-03-27 15:10:45 +0200

    poprawki do 24.03.2023

--- zadanie 24.03.2023/zarowka.js
+++ zadanie 24.03.2023/zarowka.js
@@ -1,12 +1,13 @@
+document.getElementById('zarowkazdj')
 document.querySelector("#dwa").addEventListener("click", function () {
     document.getElementById("zarowkazdj").src="zarowka1.jpg";
     document.getElementById("radiodwa").checked="false";
-    document.getElementById("checkboxjeden").checked="false";
+
 });
 document.querySelector("#jeden").addEventListener("click", function () {
     document.getElementById("zarowkazdj").src="zarowka2.jpg";
     document.getElementById("radiojeden").checked="true";
-    document.getElementById("checkboxjeden").checked="true";
+
 });
 document.querySelector('#checkboxjeden').addEventListener('click', function (){
     document.getElementById('zarowkazdj').src="zarowka2.jpg"
@@ -16,4 +17,22 @@ document.querySelector("#radiojeden").addEventListener("click", function () {
 });
 document.querySelector("#radiodwa").addEventListener("click", function () {
     document.getElementById("zarowkazdj").src = "zarowka1.jpg";
+});
+
+document.getElementById('checkbox_switch_label').innerText = "Wyłączona";
+document.getElementById('checkboxjeden').checked = false;
+
+document.getElementById('checkboxjeden').addEventListener('change',function () {
+    document.getElementById('checkboxjeden').toggleAttribute('checked');
+    if (document.getElementById('checkboxjeden').checked) {
+        img.src = 'zarowka2.jpg';
+        document.getElementById('checbox_switch_label').innerText = 'Włączone'
+        console.log('Żarówka została włączona checkboxkem');
+        document.getElementById('radiojeden').checked = true;
+    } else {
+        img.src = 'zarowka1.jpg';
+        document.getElementById('checbox_switch_label').innerText = 'Wyłączona';
+        console.log('Żarówka zostałą wyłączona checkboxem');
+        document.getElementById('radiodwa').checked = true;
+    }
 });
