3aef18888e92415f42717e8813fc0e17a668a5aa
MrFlopek <milolajfilipczak@gmail.com>
2023-04-24 15:34:09 +0200

    17.04.2023

---
+++ Zadanie 24.04.2023/Funckjematematyczne.js
@@ -0,0 +1,24 @@
+let funckja1 = Math.abs(-6.5);
+let funckja2 = Math.abs(4.3);
+let funckja3 = Math.ceil(-6.5);
+let funckja4 = Math.ceil(4.3);
+let funckja5 = Math.floor(-6.5);
+let funckja6 = Math.floor(4.3);
+let funckja7 = Math.max(-6.5,4.3);
+let funckja8 = Math.min(-6.5,4.3);
+let funckja9 = Math.pow(-6.5,2);
+let funckja10 = Math.pow(4.3,2);
+let funckja11 = Math.round(-6.5);
+let funckja12 = Math.sqrt(-6.5);
+let funckja13 = Math.sqrt(4.3);
+console.log(funckja1, funckja2, funckja3, funckja4, funckja5, funckja6, funckja7, funckja8, funckja9, funckja10, funckja11, funckja12, funckja13);
+document.getElementById('jeden').innerHTML = funckja1,funckja2;
+document.getElementById('dwa').innerHTML = funckja3,funckja4;
+document.getElementById('trzy').innerHTML = funckja5,funckja6;
+document.getElementById('cztery').innerHTML = funckja7,funckja8;
+document.getElementById('piec').innerHTML = miesiac;
+document.getElementById('szesc').innerHTML = miesiac;
+document.getElementById('siedem').innerHTML = miesiac;
+document.getElementById('osiem').innerHTML = miesiac;
+document.getElementById('dziewiec').innerHTML = miesiac;
+
