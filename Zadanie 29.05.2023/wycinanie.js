06e23cf200fd502dbb53d1193738ef46690a2611
MrFlopek <milolajfilipczak@gmail.com>
2023-06-05 15:29:29 +0200

    05.06.2023

--- Zadanie 29.05.2023/wycinanie.js
+++ Zadanie 29.05.2023/wycinanie.js
@@ -31,4 +31,41 @@ drugie.addEventListener('click', function (){
     alert(tekstjeden.substr(37, ));
 });
 
-//
+// replace
+var tekstdwa = document.getElementsByClassName('wycinanie')[0].innerText;
+var zastapienie = document.getElementById('przyciskpierw');
+zastapienie.addEventListener('click',function (){
+    var wynik = tekstdwa.replace("Microsoft", "Januszex")
+    document.getElementsByClassName("wycinanie")[0].innerHTML=wynik;
+});
+var slowo = document.getElementById('przyciskdwa');
+slowo.addEventListener('click', function () {
+   var wynik =  tekstdwa.replace('Edge','Janusz Explorer')
+    document.getElementsByClassName("wycinanie")[0].innerHTML=wynik;
+});
+
+// replaceall
+var teksttrzy = document.getElementsByClassName('zamienienie')[0].innerText;
+var zastapywanie = document.getElementById('przycisk');
+zastapywanie.addEventListener('click',function (){
+    var wynik = teksttrzy.replaceAll("Microsoft", "JanuszPol")
+    document.getElementsByClassName("zamienienie")[0].innerHTML=wynik;
+});
+
+// toUpperCase / toLowerCase
+var tekstcztery = document.getElementsByClassName('zmiana')[0].innerText;
+var wielkosc = document.getElementById('przycisktrzy');
+wielkosc.addEventListener('click',function (){
+    var wyniczek = tekstcztery.toUpperCase()
+    document.getElementsByClassName("zmiana")[0].innerHTML=wyniczek;
+});
+var male = document.getElementById('przyciskcztery');
+male.addEventListener('click', function () {
+    var wyniczek =  tekstcztery.toLowerCase()
+    document.getElementsByClassName("zmiana")[0].innerHTML=wyniczek;
+});
+var podstawa = document.getElementById('przyciskpiec');
+podstawa.addEventListener('click', function () {
+    tekstcztery.fontsize()
+    document.getElementsByClassName("zmiana")[0].innerHTML=tekstcztery;
+});
